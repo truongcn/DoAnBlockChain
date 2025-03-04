d7440668b96745d5287d48d259380fbfae213b8f
truongcn <truongvyn04@gmail.com>
2025-03-04 21:44:39 +0700

    start

--- blockchain.py
+++ blockchain.py
@@ -1,38 +1,88 @@
-import hashlib as hasher
-import datetime as date
+import hashlib
+import json
+import time
 
 class Block:
-  def __init__(self, index, timestamp, data, previous_hash):
-    self.index = index
-    self.timestamp = timestamp
-    self.data = data
-    self.previous_hash = previous_hash
-    self.hash = self.hash_block()
+    def __init__(self, index, previous_hash, timestamp, data, hash):
+        self.index = index  # Vị trí của khối trong chuỗi
+        self.previous_hash = previous_hash  # Hash của khối trước đó
+        self.timestamp = timestamp  # Thời gian tạo khối
+        self.data = data  # Dữ liệu lưu trong khối (ví dụ: giao dịch)
+        self.hash = hash  # Hash của khối hiện tại
 
-  def hash_block(self):
-    sha = hasher.sha256()
-    sha.update(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash))
-    return sha.hexdigest()
+    @staticmethod
+    def calculate_hash(index, previous_hash, timestamp, data):
+        # Tạo hash từ các thuộc tính của khối
+        value = str(index) + str(previous_hash) + str(timestamp) + str(data)
+        return hashlib.sha256(value.encode('utf-8')).hexdigest()
 
-def create_genesis_block():
-    return Block(0, date.datetime.now(), "Genesis Block", "0")
+class Blockchain:
+    def __init__(self):
+        # Khởi tạo chuỗi với khối genesis (khối đầu tiên)
+        self.chain = []
+        self.create_genesis_block()
 
-def next_block(last_block):
-    this_index = last_block.index + 1
-    this_timestamp = date.datetime.now()
-    this_data = "Hello world! I'm block" + str(this_index)
-    this_hash = last_block.hash
-    return Block(this_index, this_timestamp, this_data, this_hash)
+    def create_genesis_block(self):
+        # Tạo khối genesis
+        timestamp = time.time()
+        genesis_block = Block(0, "0", timestamp, "Genesis Block", "")
+        genesis_block.hash = Block.calculate_hash(0, "0", timestamp, "Genesis Block")
+        self.chain.append(genesis_block)
 
-blockchain = [create_genesis_block()]
-previous_block = blockchain[0]
+    def get_latest_block(self):
+        # Lấy khối mới nhất trong chuỗi
+        return self.chain[-1]
 
-num_of_blocks_to_add = 20
+    def add_block(self, data):
+        # Thêm khối mới vào chuỗi
+        previous_block = self.get_latest_block()
+        index = previous_block.index + 1
+        timestamp = time.time()
+        hash = Block.calculate_hash(index, previous_block.hash, timestamp, data)
+        new_block = Block(index, previous_block.hash, timestamp, data, hash)
+        self.chain.append(new_block)
 
-for i in range(0, num_of_blocks_to_add):
-  block_to_add = next_block(previous_block)
-  blockchain.append(block_to_add)
-  previous_block = block_to_add
+    def is_chain_valid(self):
+        # Kiểm tra tính hợp lệ của chuỗi
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
 
-print ("Block #{} da duoc them vao blockchain".format(block_to_add.index))
-print ("Hash: {}\n".format(block_to_add.hash))
+            # Kiểm tra hash của khối hiện tại
+            if current_block.hash != Block.calculate_hash(
+                current_block.index,
+                current_block.previous_hash,
+                current_block.timestamp,
+                current_block.data
+            ):
+                return False
+
+            # Kiểm tra liên kết với khối trước
+            if current_block.previous_hash != previous_block.hash:
+                return False
+        return True
+
+# Thử nghiệm Blockchain
+if __name__ == "__main__":
+    # Tạo một blockchain mới
+    my_blockchain = Blockchain()
+
+    # Thêm một số khối
+    my_blockchain.add_block("Giao dịch 1: A gửi B 10 coin")
+    my_blockchain.add_block("Giao dịch 2: B gửi C 5 coin")
+
+    # In thông tin chuỗi
+    for block in my_blockchain.chain:
+        print(f"Index: {block.index}")
+        print(f"Previous Hash: {block.previous_hash}")
+        print(f"Timestamp: {block.timestamp}")
+        print(f"Data: {block.data}")
+        print(f"Hash: {block.hash}")
+        print("---")
+
+    # Kiểm tra tính hợp lệ
+    print(f"Blockchain hợp lệ? {my_blockchain.is_chain_valid()}")
+
+    # Thử giả mạo dữ liệu
+    my_blockchain.chain[1].data = "Giao dịch giả mạo"
+    print(f"Blockchain hợp lệ sau khi giả mạo? {my_blockchain.is_chain_valid()}")
